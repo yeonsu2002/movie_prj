b2ff9c9e9fd2d837d71827b2ab68f1631802e58b
mingi_Shin <shinmingi0621@gmail.com>
2025-06-09 23:40:04 +0900

    0609 11:40

--- src/main/java/kr/co/yeonflix/member/NonMemberDTO.java
+++ src/main/java/kr/co/yeonflix/member/NonMemberDTO.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NonMemberDTO {
-
+ 
   private int userIdx;
   private LocalDate birth;
   private String email;
